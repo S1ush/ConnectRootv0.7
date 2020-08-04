6a703f5ae9b619552366e9e5e4b39b7506fee3f1
S1ush <37140143+S1ush@users.noreply.github.com>
2020-08-04 11:20:46 +0530

    lil

--- ConnectRoot/script.js
+++ ConnectRoot/script.js
@@ -1,14 +1,8 @@
 $(function onscroll() {
     $(window).on('scroll', function () {
-<<<<<<< Updated upstream
         if ( $(window).scrollTop() > 10 ) {
             console.log("active length")
             $('.navbar').addClass('active');
-=======
-        if ( $(window).scrollTop() > 5 ) {
-            console.log("active length 1")
-            $('.navbar').addClass('active',1000,"easeIn");
->>>>>>> Stashed changes
         } else {
             console.log("disabled length 2")
             $('.navbar').removeClass('active',1000,"easeIn");
